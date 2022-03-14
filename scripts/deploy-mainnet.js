04fba4d6a5302bc9256b4716e7880bcd9468ccc4
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2022-03-14 02:38:02 -0300

    get tx deploy hash

--- scripts/deploy-mainnet.js
+++ scripts/deploy-mainnet.js
@@ -10,10 +10,13 @@ async function main() {
 		25
 	)
 
+	const tx = contract.deployTransaction
+	console.log('Deployment tx:', tx.hash)
+
 	await contract.deployed()
 	;[deployer, ...addrs] = await ethers.getSigners()
 
-	console.log('ETHSPplitter deployed to:', contract.address)
+	console.log('ETHSplitter deployed to:', contract.address)
 	console.log('Deployed by:', deployer.address)
 }
 
